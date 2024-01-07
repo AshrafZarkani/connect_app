81f96bf7205f2bd2156d55a9191438168b6525f5
Menawer <menawer.work@gmail.com>
2024-01-07 12:52:38 +0300

    push project setup

---
+++ lib/app/modules/auth/views/splashscreen.dart
@@ -0,0 +1,21 @@
+import 'package:connect_app/app/core/extensions/build_context_extinsion.dart';
+import 'package:connect_app/app/core/extensions/string_extenstion.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+            color: Colors.amber,
+            height: context.screenHeight * 0.3,
+            width: context.screenWidth * 0.2,
+            child: Center(child: Text(context.translate.login))),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:connect_app/app/config/router/my_named_routes.dart';
+import 'package:connect_app/app/config/router/router.dart';
+import 'package:connect_app/app/config/theme/my_theme.dart';
+import 'package:connect_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,23 +22,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Connect"),
-      ),
-      body: Center(child: Text("Connect ")),
+      routerConfig: AppRouter.router,
+      theme: MyTheme.lightTheme,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: const Locale('en'),
     );
   }
 }
